7c5a6ce1a217cfb3d618c5ea2107337749c16602
Abdur Rahman <44705685+AbdurRahman11668@users.noreply.github.com>
2023-10-06 22:21:00 +0600

    created route for create browser router and Private Route, and create some pages

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,1 +1,26 @@
 
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
+import PropTypes from "prop-types";
+
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext)
+    const location = useLocation();
+
+    if(loading){
+        return <span className="loading loading-infinity loading-lg"></span>
+    }
+
+    if(user){
+        return children;
+    }
+    return <Navigate state={location.pathname} to='/login'></Navigate>
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};
+
+export default PrivateRoute;
