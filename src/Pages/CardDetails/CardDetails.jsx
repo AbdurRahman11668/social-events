be6ce3c5675adde7b6ffaa706f0cc0245e2dfa59
Abdur Rahman <abdur1222.ar@gmail.com>
2023-10-06 22:11:20 +0600

    firebase configuration and navbar added

---
+++ src/Pages/CardDetails/CardDetails.jsx
@@ -0,0 +1,10 @@
+
+const CardDetails = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Card Details</h2>
+        </div>
+    );
+};
+
+export default CardDetails;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className="max-w-6xl mx-auto">
+            <h2 className="text-3xl">Home</h2>
+        </div>
+    );
+};
+
+export default Home;
